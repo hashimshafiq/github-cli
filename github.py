9a274f1ee0bafad9b08cce637d31b65b93ab7c08
hashimshafiq <hashimshafiq0@gmail.com>
2017-06-28 16:29:42 +0500

    animation added

--- github.py
+++ github.py
@@ -1,9 +1,41 @@
-from colorama import init
+from colorama import init, Style, Fore, Back
 from termcolor import colored
 import requests
-import sys
+import sys, itertools
+import threading,time
 
-init()
+class Spinner:
+    busy = False
+    delay = 0.1
+
+    @staticmethod
+    def spinning_cursor():
+        while 1: 
+            for cursor in '|/-\\': yield cursor
+
+    def __init__(self, delay=None):
+        self.spinner_generator = self.spinning_cursor()
+        if delay and float(delay): self.delay = delay
+
+    def spinner_task(self):
+        while self.busy:
+            sys.stdout.write(next(self.spinner_generator))
+            sys.stdout.flush()
+            time.sleep(self.delay)
+            sys.stdout.write('\b')
+            sys.stdout.flush()
+
+    def start(self):
+        self.busy = True
+        threading.Thread(target=self.spinner_task).start()
+
+    def stop(self):
+        self.busy = False
+        time.sleep(self.delay)
+
+
+
+init(autoreset=True)
 API_URL = 'https://api.github.com/'
 
 usage = 'python filename username'
@@ -28,7 +60,12 @@ else:
 	sys.exit()
 
 
-print(colored("Getting information",'green','on_red'))
+spinner = Spinner()
+spinner.start()
+print(Fore.GREEN + Style.BRIGHT + Fore.GREEN + "Getting information")
+time.sleep(3)
+spinner.stop()
+
 
 
 
