5c7901ddb4d23f1945cff73d57d9de4e3ec28eab
hashimshafiq <hashimshafiq0@gmail.com>
2017-06-28 15:43:19 +0500

    started

---
+++ github.py
@@ -0,0 +1,17 @@
+import requests
+
+
+API_URL = 'https://api.github.com/'
+
+usage = 'python filename username'
+example = '> python github.py hashimshafiq'
+contribute = "Contribute to this project"
+		
+
+	
+
+
+
+
+
+
