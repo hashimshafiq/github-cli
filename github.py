81d10504d2c94520ce1250f93e506ad83e6d3ae5
hashimshafiq <hashimshafiq0@gmail.com>
2017-06-28 16:03:26 +0500

    argument exceptions added

--- github.py
+++ github.py
@@ -1,11 +1,36 @@
 import requests
-
+import sys
 
 API_URL = 'https://api.github.com/'
 
 usage = 'python filename username'
 example = '> python github.py hashimshafiq'
 contribute = "Contribute to this project"
+
+def argumentError():
+	print("Argument Missing")
+	print("Usage:")
+	print(usage)
+	print("Example:")
+	print(example)
+
+
+if(len(sys.argv)==2):
+	username = sys.argv[1]
+elif(len(sys.argv)==3):
+	username = sys.argv[1]
+	reponame = sys.argv[2]
+else:
+	argumentError()
+	sys.exit()
+
+
+print("Getting information")
+
+
+
+
+ 
 		
 
 	
