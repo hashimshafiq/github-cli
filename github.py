159d8a606ed9a5a0d6281088ea923c10dbd26e87
hashimshafiq <hashimshafiq0@gmail.com>
2017-06-30 17:07:04 +0500

    list all repo call added

--- github.py
+++ github.py
@@ -2,35 +2,7 @@ from colorama import init, Style, Fore, Back
 import sys, itertools, threading, time, requests
 from pprint import pprint
 from tabulate import tabulate
-
-class Spinner:
-    busy = False
-    delay = 0.1
-
-    @staticmethod
-    def spinning_cursor():
-        while 1: 
-            for cursor in '|/-\\': yield cursor
-
-    def __init__(self, delay=None):
-        self.spinner_generator = self.spinning_cursor()
-        if delay and float(delay): self.delay = delay
-
-    def spinner_task(self):
-        while self.busy:
-            sys.stdout.write(next(self.spinner_generator))
-            sys.stdout.flush()
-            time.sleep(self.delay)
-            sys.stdout.write('\b')
-            sys.stdout.flush()
-
-    def start(self):
-        self.busy = True
-        threading.Thread(target=self.spinner_task).start()
-
-    def stop(self):
-        self.busy = False
-        time.sleep(self.delay)
+from Spinner import Spinner
 
 
 
